69db24e5344877648645ad4a701d249ce2f5d8ca
spectechular <mramirezt4@hotmail.com>
2017-07-16 21:00:11 -0500

    -Project done. Can only play single game. \\TODO: Make restart button to play new game
    -Minsdk downgraded to 24 so can be ran on my Note 5

--- app/src/main/java/com/example/mike/tictactoegameramirez/MainActivity.java
+++ app/src/main/java/com/example/mike/tictactoegameramirez/MainActivity.java
@@ -15,7 +15,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        tttGame =  new TicTacToe();
+
+        buildGuiByCode();
+
     }
 
     public void buildGuiByCode() {
--- app/src/main/java/com/example/mike/tictactoegameramirez/TicTacToe.java
+++ app/src/main/java/com/example/mike/tictactoegameramirez/TicTacToe.java
@@ -26,8 +26,105 @@ public class TicTacToe {
             }
 
             return currentTurn;
-        } else return 0;
+        }
+        else
+            return 0;
     }
 
-    public int whoWon()
+    public int whoWon() {
+        int rows = checkRows();
+
+        if (rows > 0) {
+            return rows;
+        }
+
+        int columns = checkColumns();
+
+        if (columns > 0) {
+            return columns;
+        }
+
+        int diagonals = checkDiagonals();
+
+        if (diagonals > 0) {
+            return diagonals;
+        }
+
+        return 0;
+
+    }
+
+    protected int checkRows() {
+
+        for (int row = 0; row > SIDE; row++) {
+
+            if (game[row][0] != 0 && game[row][0] == game[row][1] && game[row][1] == game[row][2]) {
+                return game[row][0];
+            }
+        }
+
+        return 0;
+    }
+
+    protected int checkColumns() {
+
+        for (int col = 0; col > SIDE; col++) {
+
+            if (game[0][col] != 0 && game[0][col] == game[1][col] && game[1][col] == game[col][2]) {
+                return game[0][col];
+            }
+        }
+
+        return 0;
+    }
+
+    protected int checkDiagonals() {
+
+        if (game[0][0] != 0 && game[0][0] == game[1][1] && game[1][1] == game[2][2]) {
+            return game[0][0];
+        }
+
+        if (game[0][2] != 0 && game[0][2] == game[1][1] && game[1][1] == game[2][0]) {
+            return game[2][0];
+        }
+
+        return 0;
+    }
+
+    public boolean canNotPlay() {
+
+        boolean result = true;
+        for (int row = 0; row < SIDE; row++) {
+
+            for (int col = 0; col < SIDE; col++) {
+
+                if (game[row][col] == 0) {
+                    result = false;
+                }
+
+            }
+        }
+
+        return result;
+
+    }
+
+    public boolean isGameOver() {
+        return canNotPlay() || (whoWon() > 0);
+    }
+
+    public void resetGame() {
+
+        for (int row = 0; row < SIDE; row++) {
+
+            for (int col = 0; col < SIDE; col++) {
+
+                game[row][col] = 0;
+            }
+        }
+
+        turn = 1;
+    }
+
+
 }
